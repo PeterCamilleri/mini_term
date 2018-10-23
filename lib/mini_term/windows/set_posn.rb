709c6aedfd4a0ea21b06af836324e35a9fa72d84
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-23 16:13:59 -0400

    Added a comment to the set_posn method.

--- lib/mini_term/windows/set_posn.rb
+++ lib/mini_term/windows/set_posn.rb
@@ -3,6 +3,7 @@
 # Give MiniTerm control of the cursor position.
 module MiniTerm
 
+  # Set the row (optional) and column of the cursor.
   def self.set_posn(row: get_cursor_row, column:)
     set_cursor_posn(row * 65536 + column)
   end

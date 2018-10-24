3e56e7380cb6e582c8b4257ab2d0e8646365bc4b
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-10-24 17:38:58 -0400

    Adding more stuff.

--- lib/mini_term/common/mapped_input.rb
+++ lib/mini_term/common/mapped_input.rb
@@ -3,4 +3,23 @@
 # Get input from the user in mapped mode. (Common Code)
 module MiniTerm
 
+  # The different keyboard maps available.
+  @maps = Hash.new {|_h, k| fail "No mapping for term type #{k.inspect}."}
+
+  # What terminal types are mapped?
+  def self.map_types
+    @map.keys
+  end
+
+  # Add a terminal mapping.
+  def self.add_map(type, &defn_block)
+    @maps[type] = Mapper.new(&defn_block)
+  end
+
+  def self.get_mapped_key(&block)
+    mapper = @maps[MiniTerm::TERM_TYPE]
+    proc = block_given? ? block : Proc.new { get_raw_char }
+    mapper.get_mapped_char(&proc)
+  end
+
 end

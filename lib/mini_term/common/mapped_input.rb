0e9411cfb5382d6103d1afbb2cdad4b5cd541c86
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-11-03 12:31:38 -0400

    Added missing comment.

--- lib/mini_term/common/mapped_input.rb
+++ lib/mini_term/common/mapped_input.rb
@@ -18,6 +18,7 @@ module MiniTerm
     @maps[type] = Mapper.new(&defn_block)
   end
 
+  # Get a mapped character. Note: The block is for testing only.
   def self.get_mapped_char(&block)
     mapper = @maps[MiniTerm::TERM_TYPE]
     proc = block_given? ? block : Proc.new { get_raw_char }

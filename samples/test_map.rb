74e422004564ea689816ef554ff512b0447df474
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-11-04 11:46:29 -0500

    More work on the mapped_key_test.

--- samples/test_map.rb
+++ samples/test_map.rb
@@ -1,4 +1,5 @@
 # A sample test map for use with the mapped_key_test program.
+# These demonstrate overriding the mappings built into the program.
 
 MiniTerm.add_map(:windows) do |map|
 
@@ -6,10 +7,22 @@ MiniTerm.add_map(:windows) do |map|
   x00 = MiniTerm::PREFIX_00
   xe0 = MiniTerm::PREFIX_E0
 
-  #The Escape key
+  #The Cancel key
   map["\e"]    = :cancel
 
   #End of Input
   map["\x1A"]  = :end_of_input
 
+  $exit_info[:windows] = [:end_of_input, "Ctrl+z"]
+end
+
+MiniTerm.add_map(:ansi) do |map|
+
+  #The Cancel key
+  map["\f"]    = :cancel
+
+  #End of Input
+  map["\ez"] = :end_of_input
+
+  $exit_info[:ansi] = [:end_of_input, "Alt+z"]
 end

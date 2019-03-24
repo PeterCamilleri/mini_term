d1ba6a54081814bcdc7cf70f014bb9ff220331e0
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-03-24 12:44:02 -0400

    New for version 0.1.1

--- lib/mini_term/version.rb
+++ lib/mini_term/version.rb
@@ -2,7 +2,7 @@
 
 # A simple, portable terminal interface object.
 module MiniTerm
-  VERSION = "0.1.0".freeze
+  VERSION = "0.1.1".freeze
 
   DESCRIPTION = "mini_term: A portable encapsulation of the console terminal.".freeze
 end

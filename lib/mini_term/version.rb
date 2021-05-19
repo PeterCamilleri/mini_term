13126aae0eaac231a01da0845d66efb0e7778f46
PeterCamilleri <peter.c.camilleri@gmail.com>
2021-05-19 11:11:25 -0400

    Updated bundler dependency for next version

--- lib/mini_term/version.rb
+++ lib/mini_term/version.rb
@@ -2,7 +2,7 @@
 
 # A simple, portable terminal interface object.
 module MiniTerm
-  VERSION = "0.1.3".freeze
+  VERSION = "0.1.4".freeze
 
   DESCRIPTION = "mini_term: A portable encapsulation of the console terminal.".freeze
 end
